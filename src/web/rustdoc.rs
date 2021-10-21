1e71a7aeee34650fa3b5672c23332aaab03ae146
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2021-10-20 22:13:21 -0700

    Serve /crate/latest/crate rather than redirect

--- src/web/rustdoc.rs
+++ src/web/rustdoc.rs
@@ -318,7 +318,10 @@ pub fn rustdoc_html_server_handler(req: &mut Request) -> IronResult<Response> {
             // to prevent cloudfront caching the wrong artifacts on URLs with loose semver
             // versions, redirect the browser to the returned version instead of loading it
             // immediately
-            return redirect(&name, &v, &req_path);
+            if url_version != Some("latest") {
+                return redirect(&name, &v, &req_path);
+            }
+            v
         }
     };
 
